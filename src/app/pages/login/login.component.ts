4dac83d823005a8f413623d62d9e71ef6dc074bd
Jerick Signapan <jericksignapan3331@gmail.com>
2024-12-23 13:14:19 +0800

    implement login UI

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../material-module';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
