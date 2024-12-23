9e2afa60f91919a646634f4a03ba9ac37503b02d
Jerick Signapan <jericksignapan3331@gmail.com>
2024-12-23 16:27:01 +0800

    add dashboard

--- src/app/core/dashboard/dashboard.component.ts
+++ src/app/core/dashboard/dashboard.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+  ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
-
+  tiles = [
+    { title: 'Total Users', content: '1,250', icon: 'people' },
+    { title: 'New Orders', content: '45', icon: 'shopping_cart' },
+    { title: 'Revenue', content: '$23,400', icon: 'attach_money' },
+  ];
 }
